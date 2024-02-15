a58b8fb28fdb17ece1806f88176e15fb57c7bfb3
LucasGC1708 <109798223+LucasGC1708@users.noreply.github.com>
2024-02-15 11:15:55 -0300

    home

--- html/index.php
+++ html/index.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
     <?php include('header.php')?>
-    
+    <section class="painel">
+        <div class="interface">
+            <div class="postar">
+                
+                <a href="index.php"><button>Nova Postagem <i class="fa-solid fa-envelope"></i></button></a>
+                
+            </div>
+            <div class="postagens">
+                <h2>Tema 1</h2>
+                <div class="informacoes">
+                    <img src="" alt="">
+                    <p>sobre</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
